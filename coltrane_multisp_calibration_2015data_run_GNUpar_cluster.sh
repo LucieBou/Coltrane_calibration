5c040377bb623a4cf962808b8a7192f0e8a9267a
LUBOU323 <lubou323@ulaval.ca@ul-val-pr-ssh02.l.ul.ca>
2025-01-27 14:05:55 -0500

    Modify to work on cluster

--- coltrane_multisp_calibration_2015data_run_GNUpar_cluster.sh
+++ coltrane_multisp_calibration_2015data_run_GNUpar_cluster.sh
@@ -4,14 +4,14 @@
 # SLURM script - Coltrane Calibration
 # -----------------------------------
 
-#SBATCH --time=00:01:00
+#SBATCH --time=12:00:00
 #SBATCH --account=def-frmap5
 #SBATCH --job-name=coltrane_calibration
 #SBATCH --partition=bigmem
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lucie.bourreau.1@ulaval.ca
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=8G
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=16G
 #SBATCH -o slurm-mem-%j.out
 #SBATCH -e slurm-mem-%j.err
 
@@ -24,8 +24,8 @@ pip install --no-index -r requirements.txt
 
 echo "Starting task"
 
-parallel -j $SLURM_CPUS_PER_TASK --colsep ',' python -u coltrane_multisp_calibration_lipids_fullness_GNUpar_2015data.py {1} {2} {3} {4} {5} {6} {7} {8} "./" "BB" :::: ./multisp_parameters_2015data_oneline_test.txt
+parallel -j $SLURM_CPUS_PER_TASK --colsep ',' python -u coltrane_multisp_calibration_lipids_fullness_GNUpar_2015data.py {1} {2} {3} {4} {5} {6} {7} {8} "./" "BB" :::: ./multisp_parameters_2015data_3000.txt
 
-python -u merge_pickle_files_beluga.py "./pickle_files/" "./" "merged_files_cluster_2015data_BBforcing_test.pkl"
+python -u merge_pickle_files.py "./pickle_files/" "./" "merged_files_cluster_2015data_BBforcing_3000sets.pkl"
 
 echo "Task done"
