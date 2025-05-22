ce9a968984fdfd7a465a5b8245afe494d9d6568f
Lucie Bourreau <96539111+LucieBou@users.noreply.github.com>
2025-05-22 11:19:23 -0400

    Add codes to run Coltrane and save all outputs to make figures with all 10 bests parameters set for each scenario

---
+++ coltrane_save_alloutputs_for_figures.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+Run Coltrane for multiple costs and save outputs
+
+@author: luciebourreau
+@date: 2024/04/03
+"""
+
+import sys
+
+sys.path.append('./model')
+
+from coltrane_params import coltrane_params
+from coltrane_forcing import coltrane_forcing
+from coltrane_population import coltrane_population
+from select_C4_C6_ind_repro import select_C4_C6_repro
+
+import numpy as np
+import pickle
+from datetime import datetime
+
+
+def run_coltrane_save_outputs(u0, I0, Ks, KsIA, maxReserveFrac, rm, preySatVersion, unique_id, species, scenario, folder_path):
+    """
+    Cost function for the Coltrane model. 
+
+    Parameters
+    ----------
+    params: dict
+        Parameters to test.
+    forcing: dict
+        Set of forcing, composed of prey and temperature (surface & deep) cycle over several years.
+    obs: Dataframe
+        Observations of one or the two traits (one or two columns).
+
+    Returns
+    -------
+    out: dict
+        Output containing the RMSE for the specified paramosome and other information.
+    """
+    
+    ## Forcing
+    forcing = coltrane_forcing("NOW", 7)
+    
+    ## Construct the paramosome with the values to test
+    params = {
+        'u0': u0,
+        'I0': I0,
+        'Ks': Ks,
+        'KsIA': KsIA,
+        'maxReserveFrac': maxReserveFrac,
+        'rm': rm,
+        'tdia_exit': list(range(30, 130, 15)),
+        'tdia_enter': list(range(250, 365, 15)),
+        'preySatVersion': preySatVersion,
+        'min_genlength_years': 1,
+        'max_genlength_years': 3,
+        'dt_spawn': 15,
+    }
+    
+    p = coltrane_params(**params)
+    
+    ## Run Coltrane to create a population and keep the time serie
+    pop, popts = coltrane_population(forcing, p, 2)
+    
+    pop_file_path = f'{folder_path}/coltrane_outputs_{species}_{scenario}_{unique_id}_pop.pkl'
+    popts_file_path = f'{folder_path}/coltrane_outputs_{species}_{scenario}_{unique_id}_popts.pkl'
+
+    with open(pop_file_path, 'wb') as file:
+        pickle.dump(pop, file)
+        
+    with open(popts_file_path, 'wb') as file:
+        pickle.dump(popts, file)
+
+if __name__ == '__main__':
+    print('Save Coltrane outputs')
+
+    run_coltrane_save_outputs(float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]), sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10], sys.argv[11])
